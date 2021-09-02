0cebf6fb9b726b722994ea66ed66cc556c3277ca
Lance <lance100@comcast.net>
2021-09-02 00:05:40 -0400

    Matched formatting of other files (missed this one)

--- wave.cpp
+++ wave.cpp
@@ -9,18 +9,18 @@
 **/
 SoundSamples * wave :: generateSamples (float frequency, float sampleRate, float duration)
 {
-  int time;
+    int time;
 
-  int size = sampleRate * duration;
-  float samples [size];
+    int size = sampleRate * duration;
+    float samples [size];
 
-  for (time = 0; time < (sampleRate * duration); time ++)
-  {
-    samples [time] = generateFunction (((frequency * time) / sampleRate));
-    //sin (((2 * PI) * frequency * time) / sampleRate);
-  }
+    for (time = 0; time < (sampleRate * duration); time ++)
+    {
+        samples [time] = generateFunction (((frequency * time) / sampleRate));
+        //sin (((2 * PI) * frequency * time) / sampleRate);
+    }
 
-  SoundSamples *result = new SoundSamples (samples, size, sampleRate);
+    SoundSamples *result = new SoundSamples (samples, size, sampleRate);
 
-  return result;
+    return result;
 }
